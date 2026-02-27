db90ee30f80e65a00f9200f8b8694b5fa87fba8d
Kishor1703 <pskishor196@gmail.com>
2026-02-27 13:47:45 +0530

    URl

--- api/index.js
+++ api/index.js
@@ -44,7 +44,7 @@ app.use(cookieParser());
 const allowedOrigins = [
   (process.env.FRONTEND_ORIGIN || "https://hire-ly.vercel.app/").replace(/\/$/, ""),
   "https://hire-ly.vercel.app/",
-  "http://localhost:3000",
+  "http://localhost:3000","http://localhost:5173/",
 ];
 const corsOptions = {
   origin(origin, callback) {
